45bb7937183ab656284cb73b1a9fe981e26891a6
Aless Paredes <paredesandro@gmail.com>
2019-05-12 16:00:47 -0600

    Mejoras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo:'/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: QuienesSomosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuienesSomosComponent,
+    NavbarComponent,
+    ServiciosComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
